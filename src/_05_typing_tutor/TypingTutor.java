c26e798d821f136c243a99b051624253d19ed141
PatPrograming <30586247+PatPrograming@users.noreply.github.com>
2020-09-26 11:29:26 -0700

    :key:

--- src/_05_typing_tutor/TypingTutor.java
+++ src/_05_typing_tutor/TypingTutor.java
@@ -1,16 +1,19 @@
 package _05_typing_tutor;
 
+import java.awt.Color;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.Random;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class TypingTutor implements KeyListener {
 	static char currentLetter;
 	static JFrame frame=new JFrame();
 	static JLabel label=new JLabel();
+	static JPanel panel=new JPanel();
 public static void main(String[] args) {
 	currentLetter = generateRandomLetter();
 	label.setText(currentLetter+"");
@@ -21,9 +24,10 @@ public static void main(String[] args) {
 }
 void method() {
 	frame.addKeyListener(this);
-	frame.add(label);
-	frame.pack();
+	panel.add(label);
+	frame.add(panel);
 	frame.setVisible(true);
+	frame.pack();
 }
 static char generateRandomLetter() {
     Random r = new Random();
@@ -32,7 +36,13 @@ static char generateRandomLetter() {
 @Override
 public void keyTyped(KeyEvent e) {
 	// TODO Auto-generated method stub
-	
+	if (e.getKeyChar()==currentLetter){
+		System.out.println("Correct");
+		panel.setBackground(Color.GREEN);
+	}
+	else {
+		panel.setBackground(Color.red);
+	}
 }
 @Override
 public void keyPressed(KeyEvent e) {
